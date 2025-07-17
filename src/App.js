37b27509dad5a58e19304dc4ddbb506e857eff67
shivam-user70 <shivamshete1997@gmail.com>
2025-07-17 17:45:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="hero">
+      <div className="content">
+        <h1>Welcome to Shivam Shete's Portfolio</h1>
+        <p>I am Shivam Shete, a passionate DevOps Engineer, crafting automated, scalable, and reliable systems.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
